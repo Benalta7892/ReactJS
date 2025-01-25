9d15d306b37665eb2f9ec349a1b39c5f25e5a7a3
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-01-25 16:17:39 +0100

    Les evenements dans React et changement de state

--- appViaCRA/cars/src/App.js
+++ appViaCRA/cars/src/App.js
@@ -15,12 +15,33 @@ class App extends Component {
     });
   };
 
+  changeViaParam = (titre) => {
+    this.setState({
+      titre: titre,
+    });
+  };
+
+  changeViaBind = (param) => {
+    this.setState({
+      titre: param,
+    });
+  };
+
+  changeViaInput = (e) => {
+    this.setState({
+      titre: e.target.value,
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <Mycars title={this.state.titre} />
 
         <button onClick={this.changeTitle}>Changer le nom en dur</button>
+        <button onClick={() => this.changeViaParam("Titre via un parametre")}>Changer le nom via parametre</button>
+        <button onClick={this.changeViaBind.bind(this, "Titre via Bind")}>Via un Bind</button>
+        <input type="text" onChange={this.changeViaInput} value={this.state.titre} />
       </div>
     );
   }
