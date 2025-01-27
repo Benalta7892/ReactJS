42d5dbca981c0305b0a8efc208d7155a757f0670
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-01-27 17:30:52 +0100

    Le CSS dans React - styled-components

--- formulaire/forms/src/Forms.js
+++ formulaire/forms/src/Forms.js
@@ -1,13 +1,25 @@
 import { Component } from "react";
-import "./myCss.css";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: red;
+  font-size: 80px;
+`;
+
+const Button = styled.button`
+  background: black;
+  color: #ffffff;
+  padding: 12px 14px;
+  font-size: 15px;
+`;
 
 class Form extends Component {
   render() {
     return (
       <div>
-        <h1>Commentaire 1</h1>
-
-        <button className="btn btn-danger">Valider</button>
+        <Title>Commentaire 1</Title>
+        <button>Valider</button>
+        <Button>Valider 2</Button>
       </div>
     );
   }
