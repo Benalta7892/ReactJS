e29994b13b4059de6e4d84f08de27c5b56284d18
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-01-27 16:57:42 +0100

    Le CSS dans React - module

--- formulaire/forms/src/Forms.js
+++ formulaire/forms/src/Forms.js
@@ -1,14 +1,16 @@
 import { Component } from "react";
 import "./myCss.css";
+import MyHead from "./myHeaderOne";
+import styles from "./myCss.module.css";
 
 class Form extends Component {
   render() {
-    const myClass = this.props.head ? "blue" : "red";
-
     return (
       <div>
-        <h1 className="blue">Commentaire</h1>
-        <p className={`${myClass} bigFont`}>Je suis rouge ou bleu</p>
+        <h1 className={styles.green}>Commentaire 1</h1>
+        <MyHead />
+
+        <p className="red">Je suis rouge ou bleu</p>
         <button>Valider</button>
       </div>
     );
---
+++ formulaire/forms/src/myHeaderOne.js
@@ -0,0 +1,10 @@
+const MyHead = () => {
+  return (
+    <div>
+      <h1>Commentaire 2</h1>
+      <p className="red">Je suis rouge ou bleu</p>
+    </div>
+  );
+};
+
+export default MyHead;
