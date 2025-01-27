5221f78acbc9052c9433c68435e46ed7d3a88096
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-01-27 16:34:51 +0100

    Le CSS dans React - Inline

--- formulaire/forms/src/Forms.js
+++ formulaire/forms/src/Forms.js
@@ -1,10 +1,15 @@
 import { Component } from "react";
 
+const titreRouge = {
+  fontSize: "50px",
+  color: "red",
+};
+
 class Form extends Component {
   render() {
     return (
       <div>
-        <h1 style={{ fontSize: "50px", color: "red" }}>Commentaire</h1>
+        <h1 style={titreRouge}>Commentaire</h1>
         <button>Valider</button>
       </div>
     );
