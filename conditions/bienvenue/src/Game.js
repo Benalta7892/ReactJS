a80b0eed6b30c2a10773c82a59639200c766aa67
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-01-27 14:23:56 +0100

    Les conditions dans React

---
+++ conditions/bienvenue/src/Game.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+class Result extends Component {
+  state = {
+    name: "Mario",
+    winner: true,
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default Result;
