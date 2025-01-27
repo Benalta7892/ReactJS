3c120f27051ac8eac2191a2626283909f9b1ae18
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-01-27 13:56:21 +0100

    Le destructuring partie 2

--- destructuring/destruct/src/components/Display.js
+++ destructuring/destruct/src/components/Display.js
@@ -1,10 +1,15 @@
 import { Component } from "react";
+import Users from "./Singers";
 
 class Display extends Component {
   render() {
     return (
       <div>
-        <p>Hello World</p>
+        <h1>Musiciens</h1>
+        <Users name="Eric Clapton" age="74" />
+        <Users name="Jimi Hendrix" age="27" />
+        <Users name="David Gilmour" age="73" />
+        <Users name="Carlos Santana" age="71" />
       </div>
     );
   }
---
+++ destructuring/destruct/src/components/Singers.js
@@ -0,0 +1,12 @@
+const Users = (props) => {
+  const { name, age } = props;
+  console.log(name, age);
+
+  return (
+    <div>
+      <p>Chanteur : </p>
+    </div>
+  );
+};
+
+export default Users;
