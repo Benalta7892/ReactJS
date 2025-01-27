9a4e86f9cd07a1cdbd7ce97159181e094fe6599c
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-01-27 14:05:25 +0100

    Le destructuring partie 2

--- destructuring/destruct/src/components/Singers.js
+++ destructuring/destruct/src/components/Singers.js
@@ -1,12 +1,26 @@
-const Users = (props) => {
-  const { name, age } = props;
-  console.log(name, age);
+// const Users = ({ name, age }) => {
+//   console.log(name, age);
+//   return (
+//     <div>
+//       <p>Chanteur : </p>
+//     </div>
+//   );
+// };
 
-  return (
-    <div>
-      <p>Chanteur : </p>
-    </div>
-  );
-};
+// export default Users;
+
+// // Dans une class on ne peut pas utiliser "props" de la même maniere, on utilise "this" qui peut acceder a "props"
+import { Component } from "react";
+class Users extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Chanteur : {this.props.name} {this.props.age}
+        </p>
+      </div>
+    );
+  }
+}
 
 export default Users;
