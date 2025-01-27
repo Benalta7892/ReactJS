77e3285a0c61a6a2b50ef3795a72b50174062f97
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-01-27 16:10:30 +0100

    Les formulaires dans React

--- formulaire/forms/src/Forms.js
+++ formulaire/forms/src/Forms.js
@@ -4,11 +4,19 @@ import Car from "./Car";
 class Form extends Component {
   state = {
     username: "",
+    color: "",
+    colors: ["", "red", "blue", "green", "black", "pink"],
   };
 
   handlePseudo = (e) => {
     this.setState({
       username: e.target.value,
+    });
+  };
+
+  handleColor = (e) => {
+    this.setState({
+      color: e.target.value,
     });
   };
 
@@ -18,8 +26,23 @@ class Form extends Component {
         <Car color="red" height="400" />
         <h1>Commentaire</h1>
         <form>
-          <label>Pseudo</label>
-          <input type="text" value={this.state.username} onChange={this.handlePseudo} />
+          <div>
+            <label>Pseudo</label>
+            <input type="text" value={this.state.username} onChange={this.handlePseudo} />
+          </div>
+
+          <div>
+            <label>Couleur</label>
+            <select value={this.state.color} onChange={this.handleColor}>
+              {this.state.colors.map((color, index) => {
+                return (
+                  <option key={index} value={color}>
+                    {color}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
         </form>
       </div>
     );
