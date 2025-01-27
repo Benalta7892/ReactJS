32a09f0efc09fdbb28aa0b6803a1d42a60050c7d
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-01-27 14:40:18 +0100

    Les conditions dans React - operateur ternaire

--- conditions/bienvenue/src/Game.js
+++ conditions/bienvenue/src/Game.js
@@ -7,7 +7,7 @@ class Result extends Component {
   };
 
   render() {
-    return this.state.winner ? <h1>Bravo {this.state.name}</h1> : <h1>Raté !</h1>;
+    return this.state.winner && <h1>Bravo {this.state.name}</h1>;
   }
 }
 
