a200d6b6eb650934ddcc98486b204b0e9e26f58e
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-01-27 13:41:00 +0100

    Le destructuring partie 1

---
+++ destructuring/destruct/src/components/Display.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Display extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello World</p>
+      </div>
+    );
+  }
+}
+
+export default Display;
