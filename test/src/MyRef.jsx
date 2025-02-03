48af165bf5c4eeed247ee7b350413fbf5367b70e
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-03 17:52:30 +0100

    Les refs: Mise en place

---
+++ test/src/MyRef.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MyRef extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Valeur :</h1>
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+export default MyRef;
