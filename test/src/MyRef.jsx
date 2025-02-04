fd090fea62e5996f2647b3eff4a1a7ec9f44c12f
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-04 17:16:44 +0100

    Les refs

--- test/src/MyRef.jsx
+++ test/src/MyRef.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 
 class MyRef extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: "",
+    };
+
+    this.myTitle = React.createRef();
+  }
+
+  update = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
   render() {
+    console.log(this.myTitle);
     return (
       <div>
-        <h1>Valeur :</h1>
-        <input type="text" />
+        <h1 ref={this.myTitle}>Valeur : {this.state.value}</h1>
+        <input type="text" value={this.state.value} onChange={this.update} />
       </div>
     );
   }
