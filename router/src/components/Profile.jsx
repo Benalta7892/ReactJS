7c906e665854874c0a1451d699da05ba9bb99538
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-10 19:46:02 +0100

    Parametres Route

--- router/src/components/Profile.jsx
+++ router/src/components/Profile.jsx
@@ -1,25 +1,37 @@
 import { Component } from "react";
+import profileImg from "../profil.png";
 
 class Profile extends Component {
   state = {
-    id: null,
+    data: {},
   };
 
   componentDidMount() {
     const data = this.props.match.params.profileId;
 
-    this.setState({
-      id: data,
-    });
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((d) => this.setState({ data: d[data] }));
   }
 
   render() {
+    const { name, email, phone } = this.state.data;
+
     return (
       <div className="container mt-3">
         <h1>Profile</h1>
+        <img src={profileImg} alt="profile" />
 
         <ul className="list-group list-group-flush">
-          <li className="list-group-item">ID : {this.state.id}</li>
+          <li className="list-group-item">
+            <strong>Nom</strong> : {name}
+          </li>
+          <li className="list-group-item">
+            <strong>Email</strong> : {email}
+          </li>
+          <li className="list-group-item">
+            <strong>Tél</strong> : {phone}
+          </li>
         </ul>
       </div>
     );
