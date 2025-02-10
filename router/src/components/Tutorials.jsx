b1b983669928a6f8e3fe7248c82979fc77a6b793
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-10 16:57:28 +0100

    Les redirections

--- router/src/components/Tutorials.jsx
+++ router/src/components/Tutorials.jsx
@@ -1,6 +1,19 @@
-const Tutorials = () => {
+const Tutorials = (props) => {
+  console.log(props);
+
+  setTimeout(() => {
+    props.history.push("/");
+  }, 5000);
+
   return (
-    <div className="container">
+    <div className="container mt-3">
+      <div className="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Oups, </strong> cette page est en construction. Vous allez être redirigé vers la page d'accueil "Docs"
+        dans 5 secondes.
+        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
       <h1>Tutoriel : intro à React</h1>
     </div>
   );
