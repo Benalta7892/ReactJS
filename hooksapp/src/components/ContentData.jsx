5ea3928c3c3746e863feb8ffdad6ceac23703e57
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-11 15:51:50 +0100

    Les Hooks: useContext

--- hooksapp/src/components/ContentData.jsx
+++ hooksapp/src/components/ContentData.jsx
@@ -1,25 +1,17 @@
 import { UserContext, ColorContext } from "./MyContexts";
+import { useContext } from "react";
 
 const ContentData = () => {
+  const user = useContext(UserContext);
+  const color = useContext(ColorContext);
+
   return (
-    <UserContext.Consumer>
-      {(user) => {
-        return (
-          <ColorContext.Consumer>
-            {(color) => {
-              return (
-                <div style={{ color: color }}>
-                  <ul>
-                    <li>Nom : {user.name}</li>
-                    <li>Age : {user.age}</li>
-                  </ul>
-                </div>
-              );
-            }}
-          </ColorContext.Consumer>
-        );
-      }}
-    </UserContext.Consumer>
+    <div style={{ color: color }}>
+      <ul>
+        <li>Nom : {user.name}</li>
+        <li>Age : {user.age}</li>
+      </ul>
+    </div>
   );
 };
 
