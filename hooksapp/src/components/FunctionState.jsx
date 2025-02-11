c6c699e15d98bd7b20f4f380eb93f1428c9cdacc
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-11 11:37:49 +0100

    Les Hooks: useState - partie 1

--- hooksapp/src/components/FunctionState.jsx
+++ hooksapp/src/components/FunctionState.jsx
@@ -3,14 +3,10 @@ import { useState } from "react";
 const FunctionState = () => {
   const [count, setCount] = useState(0);
 
-  const addOne = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
-
   return (
     <div>
       <p>Function State : {count}</p>
-      <button onClick={addOne}>+ 1</button>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>+ 1</button>
     </div>
   );
 };
