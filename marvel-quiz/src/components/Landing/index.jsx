433abc6392f7ef56c665a37bc1ed479e36fcc94c
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-13 17:34:12 +0100

    Marvel quiz : Les boutons sur Landing et animation Wolverine

--- marvel-quiz/src/components/Landing/index.jsx
+++ marvel-quiz/src/components/Landing/index.jsx
@@ -1,4 +1,33 @@
+import { useRef, useEffect, useState } from "react";
+
 const Landing = () => {
-  return <main className="welcome-page"></main>;
+  const [btn, setBtn] = useState(false);
+  console.log(btn);
+  const refWolverine = useRef(null);
+
+  useEffect(() => {
+    refWolverine.current.classList.add("startingImg");
+    setTimeout(() => {
+      refWolverine.current.classList.remove("startingImg");
+      setBtn(true);
+    }, 1000);
+  }, []);
+
+  const displayBtn = btn && (
+    <>
+      <div className="left-box">
+        <button className="btn-welcome">Inscription</button>
+      </div>
+      <div className="right-box">
+        <button className="btn-welcome">Connexion</button>
+      </div>
+    </>
+  );
+
+  return (
+    <main ref={refWolverine} className="welcome-page">
+      {displayBtn}
+    </main>
+  );
 };
 export default Landing;
