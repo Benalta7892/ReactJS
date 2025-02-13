2036b09fa09f86829b9c0188b4696b94cc96cb52
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-13 11:30:13 +0100

    Les Hooks: useTransition

--- hooksapp/src/App.jsx
+++ hooksapp/src/App.jsx
@@ -1,15 +1,53 @@
 import "./App.css";
-import ClickSayHello from "./components/ClickSayHello";
-import MyContacts from "./components/MyContacts";
+import Search from "./components/Search";
+import TableUsers from "./components/TableUsers";
+import { fakeUsersGenerator } from "./data/users";
+import { useState, useEffect } from "react";
 
-function App() {
+const users = fakeUsersGenerator();
+console.log(users);
+
+const App = () => {
+  const [search, setSearch] = useState("");
+  const [resultSearch, setResultSearch] = useState([]);
+
+  // // Custom hook
+  // const { data, isLoading } = useFetch("https://jsonplaceholder.typicode.com/users");
+
+  const filterUsers = () => {
+    const findUsers = users.filter((user) => {
+      return Object.values(user).join(" ").toLocaleLowerCase().includes(search.toLocaleLowerCase());
+    });
+
+    setResultSearch(findUsers);
+  };
+
+  useEffect(() => {
+    if (search !== "") {
+      // Filter
+      filterUsers();
+    } else {
+      setResultSearch([]);
+    }
+  }, [search]);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const msgDisplay = (msg, color) => {
+    return <p style={{ textAlign: "center", color: color }}>{msg}</p>;
+  };
+
   return (
     <div className="App">
       <h1>Mes contacts</h1>
-      <MyContacts />
-      {/* <ClickSayHello /> */}
+      <p>Total : {users.length} membres</p>
+      <Search searchStr={search} searchHandler={handleChange} />
+
+      {search === "" ? null : <TableUsers dataArray={resultSearch} />}
     </div>
   );
-}
+};
 
 export default App;
--- hooksapp/src/components/TableUsers.jsx
+++ hooksapp/src/components/TableUsers.jsx
@@ -10,17 +10,17 @@ const TableUsers = ({ dataArray }) => {
         </tr>
       </thead>
       <tbody>
-        {dataArray.map(({ id, name, username, email, address }) => {
+        {dataArray.map((user) => {
           return (
-            <tr key={id}>
-              <td>{name}</td>
-              <td>{username}</td>
-              <td>{email}</td>
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
               <td>
-                {address.street}
-                {address.suite}
-                {address.city}
-                {address.zipcode}
+                {user.address.street}
+                {user.address.suite}
+                {user.address.city}
+                {user.address.zipcode}
               </td>
             </tr>
           );
@@ -30,3 +30,38 @@ const TableUsers = ({ dataArray }) => {
   );
 };
 export default TableUsers;
+
+// Table usefetch destructuring
+
+// const TableUsers = ({ dataArray }) => {
+//   return (
+//     <table id="table">
+//       <thead>
+//         <tr>
+//           <th>Nom</th>
+//           <th>Nom Utilisateur</th>
+//           <th>Email</th>
+//           <th>Adresse</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {dataArray.map(({ id, name, username, email, address }) => {
+//           return (
+//             <tr key={id}>
+//               <td>{name}</td>
+//               <td>{username}</td>
+//               <td>{email}</td>
+//               <td>
+//                 {address.street}
+//                 {address.suite}
+//                 {address.city}
+//                 {address.zipcode}
+//               </td>
+//             </tr>
+//           );
+//         })}
+//       </tbody>
+//     </table>
+//   );
+// };
+// export default TableUsers;
