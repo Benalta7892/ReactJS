fa6d02e8d1691f79d50da0caac2d4eb963e31dff
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-13 15:41:36 +0100

    PropsTypes: Exercice CustomButton - CustomTable avec les propTypes

--- propsTypes/src/components/Vegeta.jsx
+++ propsTypes/src/components/Vegeta.jsx
@@ -2,6 +2,8 @@ import { Component } from "react";
 import vegeta from "../assets/vegeta.jpg";
 import countHits from "../hoc/countHits";
 import { PropTypes } from "prop-types";
+import CustomBtn from "../customComponents/CustomBtn";
+import CustomTable from "../customComponents/CustomTable";
 
 class Vegeta extends Component {
   // static propTypes = {
@@ -19,37 +21,21 @@ class Vegeta extends Component {
         <td>{life} %</td>
       ) : (
         <td>
-          <span className="badge badge-danger">Mort</span>
+          <span className="badge bg-danger">Mort</span>
         </td>
-      );
-
-    const button =
-      life > 0 ? (
-        <button onClick={addOneHit} className="btn btn-success m-3">{`${name} Frappe`}</button>
-      ) : (
-        <button className="btn btn-danger m-3 disabled">Mort</button>
       );
 
     return (
       <div className="col">
         <img src={vegeta} alt="Vegeta" height="400px" />
         <br />
-        {button}
+        {life > 0 ? (
+          <CustomBtn clickHandler={addOneHit} classes="btn btn-success m-3">{`${name} Frappe`}</CustomBtn>
+        ) : (
+          <CustomBtn classes="btn btn-danger m-3 disabled">Mort</CustomBtn>
+        )}
 
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col"> Coups</th>
-              <th scope="col"> Vie</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{hocState.hits}</td>
-              {lifeValue}
-            </tr>
-          </tbody>
-        </table>
+        <CustomTable hocStateObjProp={hocState} lifeValueProp={lifeValue} />
       </div>
     );
   }
