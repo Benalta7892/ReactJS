6f9ea3bb5a81036f8b50a102c56db746d1e7cfc1
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-14 18:09:52 +0100

    Configuration de Firebase

--- marvel-quiz/src/components/Firebase/firebaseConfig.js
+++ marvel-quiz/src/components/Firebase/firebaseConfig.js
@@ -1,12 +1,32 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
+// class Firebase {
+//   constructor() {
+//     app.initializeApp(config);
+//     this.auth = app.auth();
+//   }
+// }
+
+// // Inscription
+// signupUser = (email, password) => this.auth.createUserWithEmailAndPassword(email, password);
+
+// // Connexion
+// loginUser = (email, password) => this.auth.signInWithEmailAndPassword(email, password);
+
+// // Deconnexion
+// signoutUser = () => this.auth.signOut();
+
 const app = initializeApp(config);
+export const auth = getAuth(app);
+
+// export default app;
