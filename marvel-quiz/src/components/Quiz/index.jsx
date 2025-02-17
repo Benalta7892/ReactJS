2829680dbedf00e9c6f4f5d87d20a05fec123de6
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-17 11:48:30 +0100

    Recuperer de la data de Cloud Firestore

--- marvel-quiz/src/components/Quiz/index.jsx
+++ marvel-quiz/src/components/Quiz/index.jsx
@@ -1,4 +1,14 @@
-const Quiz = () => {
-  return <div>Quiz</div>;
-};
+import { Component } from "react";
+
+class Quiz extends Component {
+  render() {
+    const { pseudo } = this.props.userData;
+
+    return (
+      <div>
+        <h2>Pseudo : {pseudo}</h2>
+      </div>
+    );
+  }
+}
 export default Quiz;
--- marvel-quiz/src/components/Welcome/index.jsx
+++ marvel-quiz/src/components/Welcome/index.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../Firebase/firebaseConfig";
+import { auth, user } from "../Firebase/firebaseConfig";
+import { getDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import Logout from "../Logout";
 import Quiz from "../Quiz";
@@ -9,11 +10,27 @@ const Welcome = () => {
   const navigate = useNavigate();
 
   const [userSession, setUserSession] = useState(null);
+  const [userData, setUserData] = useState({});
 
   useEffect(() => {
     const listener = onAuthStateChanged(auth, (user) => {
       user ? setUserSession(user) : navigate("/");
     });
+
+    if (!!userSession) {
+      const colRef = user(userSession.uid);
+
+      getDoc(colRef)
+        .then((snapshot) => {
+          if (snapshot.exists()) {
+            const docData = snapshot.data();
+            setUserData(docData);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
 
     return listener;
   }, [userSession, navigate]);
@@ -26,7 +43,7 @@ const Welcome = () => {
     <div className="quiz-bg">
       <div className="container">
         <Logout />
-        <Quiz />
+        <Quiz userData={userData} />
       </div>
     </div>
   );
