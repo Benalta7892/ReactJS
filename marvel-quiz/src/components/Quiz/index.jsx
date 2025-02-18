1c89e22448106b485a1f858c21030ed15e4ede33
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-18 10:50:35 +0100

    Recuperer les questions a poser dans le quiz - 1

--- marvel-quiz/src/components/Quiz/index.jsx
+++ marvel-quiz/src/components/Quiz/index.jsx
@@ -1,8 +1,44 @@
 import { Component } from "react";
 import Levels from "../Levels";
 import ProgressBar from "../ProgressBar";
+import { QuizMarvel } from "../quizMarvel";
 
 class Quiz extends Component {
+  state = {
+    levelNames: ["debutant", "confirme", "expert"],
+    quizLevel: 0,
+    maxQuestions: 10,
+    storedQuestions: [],
+    question: null,
+    options: [],
+  };
+
+  loadQuestions = (level) => {
+    const fetchedArrayQuiz = QuizMarvel[0].quizz[level];
+
+    if (fetchedArrayQuiz.length >= this.state.maxQuestions) {
+      const newArray = fetchedArrayQuiz.map(({ answer, ...keepRest }) => keepRest);
+      this.setState({
+        storedQuestions: newArray,
+      });
+    } else {
+      console.log("Pas assz de questions !");
+    }
+  };
+
+  componentDidMount() {
+    this.loadQuestions(this.state.levelNames[this.state.quizLevel]);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.storedQuestions !== prevState.storedQuestions) {
+      this.setState({
+        question: null,
+        options: [],
+      });
+    }
+  }
+
   render() {
     // const { pseudo } = this.props.userData;
 
