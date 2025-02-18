f1736856a1b81ecec6ed2a901ab849393787520d
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-18 11:27:31 +0100

    Validation de la reponse et gestion du score

--- marvel-quiz/src/components/Quiz/index.jsx
+++ marvel-quiz/src/components/Quiz/index.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import Levels from "../Levels";
 import ProgressBar from "../ProgressBar";
 import { QuizMarvel } from "../quizMarvel";
@@ -14,12 +14,17 @@ class Quiz extends Component {
     idQuestion: 0,
     btnDisabled: true,
     userAnswer: null,
+    score: 0,
   };
+
+  storedDataRef = React.createRef();
 
   loadQuestions = (level) => {
     const fetchedArrayQuiz = QuizMarvel[0].quizz[level];
 
     if (fetchedArrayQuiz.length >= this.state.maxQuestions) {
+      this.storedDataRef.current = fetchedArrayQuiz;
+
       const newArray = fetchedArrayQuiz.map(({ answer, ...keepRest }) => keepRest);
       this.setState({
         storedQuestions: newArray,
@@ -40,7 +45,33 @@ class Quiz extends Component {
         options: this.state.storedQuestions[this.state.idQuestion].options,
       });
     }
+
+    if (this.state.idQuestion !== prevState.idQuestion) {
+      this.setState({
+        question: this.state.storedQuestions[this.state.idQuestion].question,
+        options: this.state.storedQuestions[this.state.idQuestion].options,
+        userAnswer: null,
+        btnDisabled: true,
+      });
+    }
   }
+
+  nextQuestion = () => {
+    if (this.state.idQuestion === this.state.maxQuestions - 1) {
+      // end
+    } else {
+      this.setState((prevState) => ({
+        idQuestion: prevState.idQuestion + 1,
+      }));
+    }
+
+    const goodAnswer = this.storedDataRef.current[this.state.idQuestion].answer;
+    if (this.state.userAnswer === goodAnswer) {
+      this.setState((prevState) => ({
+        score: prevState.score + 1,
+      }));
+    }
+  };
 
   submitAnswer = (selectedAnswer) => {
     this.setState({
@@ -72,7 +103,7 @@ class Quiz extends Component {
 
         {displayOptions}
 
-        <button disabled={this.state.btnDisabled} className="btn-submit">
+        <button onClick={this.nextQuestion} disabled={this.state.btnDisabled} className="btn-submit">
           Suivant
         </button>
       </div>
