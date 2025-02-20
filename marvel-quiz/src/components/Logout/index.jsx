a958c68a4376bc40b27845573c757bd7cd04dbad
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-20 19:13:37 +0100

    Afficher une info-bulle via react-tooltip

--- marvel-quiz/src/components/Logout/index.jsx
+++ marvel-quiz/src/components/Logout/index.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../Firebase/firebaseConfig";
 import { useNavigate } from "react-router-dom";
+import { Tooltip } from "react-tooltip";
 
 const Logout = () => {
   const navigate = useNavigate();
@@ -31,8 +32,9 @@ const Logout = () => {
     <div className="logout-container">
       <label className="switch">
         <input onChange={handleChange} type="checkbox" checked={checked} />
-        <span className="slider round"></span>
+        <span className="slider round" data-tooltip-id="my-tooltip" data-tooltip-content="Déconnexion"></span>
       </label>
+      <Tooltip id="my-tooltip" place="left" effect="solid" />
     </div>
   );
 };
