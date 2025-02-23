53a06cb091b0419967e62327394bdf95ce002459
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-23 10:35:25 +0100

    Gestion de plusieurs actions

--- redux/script.js
+++ redux/script.js
@@ -1,5 +1,6 @@
 // ACTION
 const BUY_PHONE = "BUY_PHONE";
+const BUY_TABLET = "BUY_TABLET";
 
 function buyPhone() {
   return {
@@ -7,11 +8,17 @@ function buyPhone() {
   };
 }
 
+function buyTablet() {
+  return {
+    type: BUY_TABLET,
+  };
+}
+
 // Reducer
-// (prevState, action) => newState
 
 const initialState = {
   phones: 5,
+  tablets: 10,
 };
 
 const reducer = (state = initialState, action) => {
@@ -21,18 +28,35 @@ const reducer = (state = initialState, action) => {
         ...state,
         phones: state.phones - 1,
       };
+    case BUY_TABLET:
+      return {
+        ...state,
+        tablets: state.tablets - 1,
+      };
     default:
       return state;
   }
 };
 
+// Creer le store
 const store = Redux.createStore(reducer);
-const availablePhones = document.getElementById("count");
+
+// Recuperer la data du Store
+const availablePhones = document.getElementById("count-phone");
+const availableTablets = document.getElementById("count-tab");
 availablePhones.innerHTML = store.getState().phones;
+availableTablets.innerHTML = store.getState().tablets;
+
+// Effectuer le Dispatch d'une action
 document.getElementById("buy-phone").addEventListener("click", function () {
   store.dispatch(buyPhone());
 });
+document.getElementById("buy-tablet").addEventListener("click", function () {
+  store.dispatch(buyTablet());
+});
 
+// Listerner
 store.subscribe(() => {
   availablePhones.innerHTML = store.getState().phones;
+  availableTablets.innerHTML = store.getState().tablets;
 });
