f71cf2a0726ecb22d78dde4c69f394f59efa3889
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-24 17:28:17 +0100

    Connect HOC et mapStateToProps

--- shop/src/components/PhoneComponent.jsx
+++ shop/src/components/PhoneComponent.jsx
@@ -1,14 +1,22 @@
 import phone from "../images/phone.png";
+import { connect } from "react-redux";
 
-function PhoneComponent() {
+function PhoneComponent(props) {
   return (
     <div className="container">
       <img src={phone} alt="phone" />
       <p>
-        Disponibilité :<span id="count-phone"></span>
+        Disponibilité : <span id="count-phone">{props.phones}</span>
       </p>
       <button>Acheter</button>
     </div>
   );
 }
-export default PhoneComponent;
+
+const mapStateToProps = (state) => {
+  return {
+    phones: state.phones,
+  };
+};
+
+export default connect(mapStateToProps)(PhoneComponent);
