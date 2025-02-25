5be3bf37614c355072043ccbe7a285ddd873a156
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-02-25 19:00:12 +0100

    useSelector Hook

--- shop/src/components/PhoneComponent.jsx
+++ shop/src/components/PhoneComponent.jsx
@@ -1,28 +1,22 @@
-/* eslint-disable react/prop-types */
+// /* eslint-disable react/prop-types */
 import phone from "../images/phone.png";
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 // import store from "../redux/store";
 import { buyPhone } from "../redux/phone/actionPhone";
 
-function PhoneComponent(props) {
-  console.log(props);
+function PhoneComponent() {
+  const phones = useSelector((state) => state.phones);
 
   return (
     <div className="container">
       <img src={phone} alt="phone" />
       <p>
-        Disponibilité : <span id="count-phone">{props.phones}</span>
+        Disponibilité : <span id="count-phone">{phones}</span>
       </p>
-      <button onClick={() => props.buyPhone()}>Acheter</button>
+      <button>Acheter</button>
     </div>
   );
 }
-
-const mapStateToProps = (state) => {
-  return {
-    phones: state.phones,
-  };
-};
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -30,4 +24,37 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(PhoneComponent);
+export default PhoneComponent;
+
+// import phone from "../images/phone.png";
+// import { connect } from "react-redux";
+// // import store from "../redux/store";
+// import { buyPhone } from "../redux/phone/actionPhone";
+
+// function PhoneComponent(props) {
+//   console.log(props);
+
+//   return (
+//     <div className="container">
+//       <img src={phone} alt="phone" />
+//       <p>
+//         Disponibilité : <span id="count-phone">{props.phones}</span>
+//       </p>
+//       <button onClick={() => props.buyPhone()}>Acheter</button>
+//     </div>
+//   );
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     phones: state.phones,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     buyPhone: () => dispatch(buyPhone()),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(PhoneComponent);
