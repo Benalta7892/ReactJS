009a082f733134b1c497b47ff9f225a2894ac6a2
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-03-03 18:33:30 +0100

    Initialisation de l'application

---
+++ books/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="footer text-white bg-secondary mt-5">
+      <div className="container p-3">
+        <span>&copy; DonkeyGeek</span>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
