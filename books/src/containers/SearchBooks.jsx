05e91824fbd26fa143b07c408be08055f06f5427
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-03-14 14:07:34 +0100

    Container Search Books

--- books/src/containers/SearchBooks.jsx
+++ books/src/containers/SearchBooks.jsx
@@ -1,4 +1,43 @@
 const SearchBooks = () => {
-  return <div>SearchBooks</div>;
+  return (
+    <main role="main">
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container text-center">
+          <h1 className="display-4">BOOKS</h1>
+          <p>Indiquez le sujet du livre à rechercher sur Google API</p>
+
+          <form className="form-inline justify-content-center">
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="Quoi rechercher ?" required />
+            </div>
+
+            <div className="form-group">
+              <button className="btn btn-outline-secondary ml-3">Rechercher</button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div className="container" style={{ minHeight: "200px" }}>
+        <div className="accordion">
+          <div className="card mb-2">
+            <div className="card-header"></div>
+            <div className="collapse" data-parent="accordion">
+              <div className="card-body">
+                {
+                  // Afficher une image si y'en a
+                  // Afficher le titre du livre
+                  // Afficher l'Auteur
+                  // Afficher la Description
+                  // Btn plus infos
+                  // Btn Enregistrer
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 export default SearchBooks;
