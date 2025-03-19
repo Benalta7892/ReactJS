c79158989c5f3990ef45fab108ddd9e26ce118ec
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-03-19 17:39:03 +0100

    Exercice 2

--- RouterV6/react-router-updated/src/components/Tutorials.jsx
+++ RouterV6/react-router-updated/src/components/Tutorials.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Tutorials() {
+  const [underConst, setUnderConst] = useState({
+    Docs: false,
+    Tutorials: true,
+    Community: false,
+  });
   const navigate = useNavigate();
+
+  setTimeout(() => {
+    navigate("/");
+  }, 5000);
 
   return (
     <div className="container">
-      <h1>Tutoriel: Intro to React</h1>
+      {underConst.Tutorials ? (
+        <div className="alert alert-warning alert-dismissible fade show mt-3" role="alert">
+          <strong>Holy guacamole!</strong> Cette page n'existe pas ! Redirection dans 5 secondes...
+          <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      ) : (
+        <h1>Tutoriel: Intro to React</h1>
+      )}
 
-      <button className="btn btn-primary" onClick={() => navigate(-1)}>
+      {/* <button className="btn btn-primary" onClick={() => navigate(-1)}>
         Retour vers Docs
       </button>
 
       <button className="btn btn-success" onClick={() => navigate("/community")}>
         Vers Community
-      </button>
+      </button> */}
     </div>
   );
 }
