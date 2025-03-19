294390f3813ea0fe4b05db1c8c2ea5e8913a99b3
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-03-19 17:26:41 +0100

    Exercice 1

--- RouterV6/react-router-updated/src/App.jsx
+++ RouterV6/react-router-updated/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Route, Routes, Navigate } from "react-router-dom";
+import { useState } from "react";
 import Docs from "./components/Docs";
 import Tutorials from "./components/Tutorials";
 import Community from "./components/Community";
@@ -7,6 +8,12 @@ import Menu from "./components/Menu";
 import ErrorPage from "./components/ErrorPage";
 
 function App() {
+  const [underConst, setUnderConst] = useState({
+    Docs: false,
+    Tutorials: true,
+    Community: false,
+  });
+
   return (
     <>
       <Menu />
@@ -15,9 +22,11 @@ function App() {
         {/* Redirection de "/" vers "/docs" */}
         <Route path="/" element={<Navigate to="/docs" replace />} />
 
+        <Route path="/tutorial" element={<Navigate to="/docs" replace />} />
+
         {/* Les routes */}
         <Route path="/docs" element={<Docs />} />
-        <Route path="/tutorial" element={<Tutorials />} />
+        <Route path="/tutorial" element={underConst.Tutorials ? <Navigate to="/docs" /> : <Tutorials />} />
         <Route path="/community" element={<Community />} />
 
         {/* Page d'erreur */}
