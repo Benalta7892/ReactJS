2e66bf28bcad1bfa112b3a0783754dd6ee420f37
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-03-19 16:00:54 +0100

    React routeur avec la derniere version

--- RouterV6/react-router-updated/src/App.jsx
+++ RouterV6/react-router-updated/src/App.jsx
@@ -1,5 +1,5 @@
 // import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Docs from "./components/Docs";
 import Tutorials from "./components/Tutorials";
 import Community from "./components/Community";
@@ -12,9 +12,15 @@ function App() {
       <Menu />
 
       <Routes>
-        <Route exact path="/" element={<Docs />} />
+        {/* Redirection de "/" vers "/docs" */}
+        <Route path="/" element={<Navigate to="/docs" />} />
+
+        {/* Les routes */}
+        <Route path="/docs" element={<Docs />} />
         <Route path="/tutorial" element={<Tutorials />} />
         <Route path="/community" element={<Community />} />
+
+        {/* Page d'erreur */}
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
