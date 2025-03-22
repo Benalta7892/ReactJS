41a9e964664f91490ac129a79f64f12d505c921e
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-03-22 13:07:14 +0100

    Recuperer une data API

--- RouterV6/react-router-updated/src/components/Profile.jsx
+++ RouterV6/react-router-updated/src/components/Profile.jsx
@@ -1,11 +1,30 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Profile = () => {
+  const [data, setData] = useState({});
   const params = useParams();
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/users/${params.id}`).then((resp) => {
+      setData(resp.data);
+    });
+  }, [params.id]);
+
+  const { name, email, phone } = data;
 
   return (
     <ul className="list-group list-group-flush">
-      <li className="list-group-item">ID Utilisateur : {params.id}</li>
+      <li className="list-group-item">
+        <strong>Nom</strong> : {name}
+      </li>
+      <li className="list-group-item">
+        <strong>Email</strong> : {email}
+      </li>
+      <li className="list-group-item">
+        <strong>Tél</strong> : {phone}
+      </li>
     </ul>
   );
 };
